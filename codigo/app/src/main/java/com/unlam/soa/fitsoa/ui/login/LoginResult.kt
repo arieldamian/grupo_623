96f13119a1f4101a0b6f8efb0dcf7999e0390fed
arieldamian <ariel.damian.gonzalez@gmail.com>
2020-05-12 20:31:25 -0300

    Login given by default.

---
+++ codigo/app/src/main/java/com/unlam/soa/fitsoa/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.unlam.soa.fitsoa.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
