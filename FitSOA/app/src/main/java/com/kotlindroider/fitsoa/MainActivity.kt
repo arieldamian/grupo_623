f660e193126ad06a2f7d6d70cf6d4a6d533de31f
Mati Dastugue <mdastugu@amazon.com>
2020-05-13 19:15:59 -0300

    Add login + register

---
+++ FitSOA/app/src/main/java/com/kotlindroider/fitsoa/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.kotlindroider.fitsoa
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(com.kotlindroider.fitsoa.R.layout.activity_main)
+
+    }
+
+}
