3d85610c129fa79e8f4212e40f8851758d5af4bd
Ariel Damian <ariel.damian.gonzalez@gmail.com>
2020-05-17 01:10:58 -0300

    Start using build constant.

--- codigo/app/src/main/java/com/unlam/soa/models/SignInBody.kt
+++ codigo/app/src/main/java/com/unlam/soa/models/SignInBody.kt
@@ -1,10 +1,12 @@
 package com.unlam.soa.models
 
+import com.unlam.soa.fitsoa.BuildConfig
+
 data class SignInBody(
                       val email: String,
                       val password: String
 ) {
-    val env: String = "TEST"
+    val env: String? = BuildConfig.API_ENV
     val commission: Int = 1234
     val group: Int = 1234
 }
--- codigo/app/src/main/java/com/unlam/soa/models/UserBody.kt
+++ codigo/app/src/main/java/com/unlam/soa/models/UserBody.kt
@@ -1,5 +1,7 @@
 package com.unlam.soa.models
 
+import com.unlam.soa.fitsoa.BuildConfig
+
 data class UserBody(
                     val name: String,
                     val lastname: String,
@@ -7,7 +9,7 @@ data class UserBody(
                     val email: String,
                     val password: String
 ) {
-    val env: String = "TEST"
+    val env: String? = BuildConfig.API_ENV
     val commission: Int = 1234
     val group: Int = 1234
 }
