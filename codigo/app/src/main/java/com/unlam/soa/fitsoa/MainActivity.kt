5ff569c782ce891c913a6c16e4dce54f81004cf7
Ariel Damian <ariel.damian.gonzalez@gmail.com>
2020-05-20 00:35:12 -0300

    Check for session onResume and onCreate.

--- codigo/app/src/main/java/com/unlam/soa/fitsoa/MainActivity.kt
+++ codigo/app/src/main/java/com/unlam/soa/fitsoa/MainActivity.kt
@@ -1,21 +1,33 @@
 package com.unlam.soa.fitsoa
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import com.unlam.soa.sharedPreferences.AppPreferences
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     private var _tokenText: TextView? = null
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        checkLogin()
+
         setContentView(R.layout.activity_main)
-
         _tokenText = findViewById<TextView>(R.id.token) as TextView
-
         _tokenText!!.text = AppPreferences.token
+    }
 
+    override fun onResume() {
+        super.onResume()
+
+        checkLogin()
+    }
+
+    private fun checkLogin() {
+        if (!AppPreferences.isLogged) {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
