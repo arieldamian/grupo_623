343323d578bca22e6e2a957b88e233d8ccf571de
Ariel Damian <ariel.damian.gonzalez@gmail.com>
2020-06-04 00:53:16 -0300

    Omg my 👀

--- codigo/app/src/main/java/com/unlam/soa/models/UserBody.kt
+++ codigo/app/src/main/java/com/unlam/soa/models/UserBody.kt
@@ -1,11 +1,11 @@
 package com.unlam.soa.models
 
 data class UserBody(
-                    val name: String,
-                    val lastname: String,
-                    val dni: Int,
-                    val email: String,
-                    val password: String
+    val name: String,
+    val lastname: String,
+    val dni: Int,
+    val email: String,
+    val password: String
 ) {
     val env: String? = com.unlam.soa.fitsoa.BuildConfig.API_ENV
     val commission: Int = 1234
